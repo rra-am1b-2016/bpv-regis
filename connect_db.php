cc4d7516c3da62134a45dae77d196ffb2f56d29f
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-03-19 17:35:45 +0100

    test

--- connect_db.php
+++ connect_db.php
@@ -2,7 +2,7 @@
    // Gebruikersnaam mysql server
    $username = "root";
 
-   // Password is niet aanwezig.
+   // Password is niet aanwezig...
    $password = "";
    $servername= "localhost";
    $databasename = "am1b_2017_diagram";
