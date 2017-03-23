412eb40aff6db6f98f6b451deef08a5df41670ec
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-03-23 13:50:56 +0100

    carousel

--- home.php
+++ home.php
@@ -4,3 +4,63 @@
 <p class="lead">Welkom op de BPV registratie-site. Je kunt hier potentiele bedrijven registreren.
    Je kunt ook de status van ieder bedrijf bijhouden.
 </p>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+      <li data-target="#myCarousel" data-slide-to="4"></li>
+      <li data-target="#myCarousel" data-slide-to="5"></li>
+      <li data-target="#myCarousel" data-slide-to="6"></li>
+      <li data-target="#myCarousel" data-slide-to="7"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="./images/bird.jpg" alt="Chania" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="./images/sesamestreet.jpg" alt="Chania" width="460" height="345">
+      </div>
+    
+      <div class="item">
+        <img src="./images/mountain.jpeg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="./images/river.jpg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="./images/pacman.png" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="./images/gitar.jpg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="./images/JulesVerne.jpg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="./images/shakespeare.jpg" alt="Flower" width="460" height="345">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
--- js-selector.php
+++ js-selector.php
@@ -1,6 +1,7 @@
 <?php
    if (isset($_GET["content"])) {
       echo "<script src='./js/" . $_GET["content"] . ".js'></script>";
-      echo "<script src='./js/" . $_GET["content"] . ".js'></script>";
+   } else {
+      echo "<script src='./js/home.js'></script>";      
    }
 ?>
